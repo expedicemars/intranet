3aee3610dd430f3d01ccfeedefda4a5827822cd6
Josef Lát <josef.latj@gmail.com>
2023-04-02 00:10:02 +0200

    ukonceni_registrace_better

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -1,4 +1,3 @@
 import httpGet from "./httpGet.js"
-let datum = JSON.parse(httpGet("send_noauth/registrace"))
-let d = new Date(datum["date"])
-document.getElementById("date").innerHTML = d.toLocaleDateString()
+let datum = httpGet("send_noauth/registrace_pretty")
+document.getElementById("date").innerHTML = datum
