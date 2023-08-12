47b50d69b84113c22e30f095c1f70cb435600087
Josef Lát <josef.latj@gmail.com>
2023-08-12 10:31:37 +0200

    prejmenovani kol

--- website/api/user_api.py
+++ website/api/user_api.py
@@ -25,7 +25,7 @@ def uzamcene_zmeny():
 
 @user_api.route("/volne_pohovory")
 @require_role_on_current_user("user")
-@require_progress_na_ucastnikovi("První kontakt")
+@require_progress_na_ucastnikovi("Motivační call")
 def volne_pohovory():
     result = []
     for p in get_neobsazene_pohovory():
@@ -39,7 +39,7 @@ def volne_pohovory():
 
 @user_api.route("/datum_pohovoru")
 @require_role_on_current_user("user")
-@require_progress_na_ucastnikovi("První kontakt")
+@require_progress_na_ucastnikovi("Motivační call")
 def datum_pohovoru():
     result = {
         "datum": pretty_datetime(current_user.datum_pohovoru),
