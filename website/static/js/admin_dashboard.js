bb3258a8280d031354f86fa09cb347670499483a
Josef Lát <josef.latj@gmail.com>
2023-11-07 12:57:10 +0100

    bugfix

--- website/static/js/admin_dashboard.js
+++ website/static/js/admin_dashboard.js
@@ -1,23 +1,10 @@
 import httpGet from "./httpGet.js"
 let emaily = httpGet("/admin_api/emaily_admin_editoru")
-let odkazy = JSON.parse(httpGet("/admin_api/odkazy"))
-let odkazy_list = document.getElementById("odkazy")
 let dostupne_odbornosti = JSON.parse(httpGet("/noauth_api/dostupne_odbornosti"))
 let tbody = document.getElementById("tbody")
 let statistiky = JSON.parse(httpGet("/admin_api/statistiky"))
 
 document.getElementById("emails").innerHTML = emaily
-
-for (let o of odkazy) {
-    let li = document.createElement("li")
-    odkazy_list.appendChild(li)
-    let a = document.createElement("a")
-    li.appendChild(a)
-    a.innerHTML = o["popis"]
-    a.href = o["odkaz"]
-    a.target = "_blank"
-    a.classList.add("link")
-}
 
 // vemu si children, appendnu neco doprostred a vratim je tam
 let children = Array.from(tbody.children)
@@ -35,7 +22,6 @@ tbody.innerHTML = null
 for (let ch of children) {
     tbody.appendChild(ch)
 }
-console.log(statistiky)
 
 
 for (let key of Object.keys(statistiky)) {
