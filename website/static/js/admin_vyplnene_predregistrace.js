27d7c1cb8fa7882c0e44ec3696ad2b5ebb501355
Josef Lát <josef.latj@gmail.com>
2023-12-21 09:38:30 +0100

    kopirovani mailu predregistrovanych do schranky

--- website/static/js/admin_vyplnene_predregistrace.js
+++ website/static/js/admin_vyplnene_predregistrace.js
@@ -18,3 +18,18 @@ for (let i = 0; i<mailing_list.length; i++) {
     btn.value = mail["email"]
     tc.make_row([mailing_list.length - i, mail["pretty"], mail["email"], btn], [0, 0, 0, 0])
 }
+
+let mails = []
+for (let m of mailing_list) {
+    mails.push(m.email)
+}
+
+
+document.getElementById("zkopirovat_button").addEventListener("click", function() {
+    let text = mails.join(", ")
+    navigator.clipboard.writeText(text)
+    this.innerText = "Hotovo"
+    setTimeout(() => {
+        this.innerText = "Znovu kopírovat"
+    }, 1000);
+})
