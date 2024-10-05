67d39a30892f2dce441b91c3d9e43ec54eb39f23
Josef Lát <josef.latj@gmail.com>
2024-10-05 21:28:51 +0200

    věk, lepší formulace

--- website/models/user.py
+++ website/models/user.py
@@ -105,10 +105,21 @@ class User(db.Model, UserMixin):
             puvod = "Česká republika"
         elif self.puvod == "sk":
             puvod = "Slovensko"
+            
+        def calculate_age(self):
+            if self.datum_narozeni:
+                today = datetime.now()
+                age = today.year - self.datum_narozeni.year
+                # Check if the birthday has already occurred this year
+                if (today.month, today.day) < (self.datum_narozeni.month, self.datum_narozeni.day):
+                    age -= 1
+                return age
+            return None
         return {
             "jmeno": self.jmeno,
             "prijmeni": self.prijmeni,
             "datum_narozeni": pretty_date(self.datum_narozeni.isoformat()) if self.datum_narozeni else None,
+            "vek": int(calculate_age(self)) if self.datum_narozeni else None,
             "rok_maturity": self.rok_maturity,
             "zeme_puvodu": puvod,
             "email": self.email,
